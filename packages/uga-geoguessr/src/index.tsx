0ed174b2b3b887b13c5f94d10f187697b639e5e1
EHansonn <evanhanson02@gmail.com>
2023-11-27 19:42:15 -0500

    smol fix

--- packages/uga-geoguessr/src/index.tsx
+++ packages/uga-geoguessr/src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
